8564e9705222c303460d5fbd552f184b197d39ec
Evan Hennis <nurl_50@hotmail.com>
2018-10-25 21:57:27 -0500

    Finished first q-learner with a discrete environment

--- qlearning.py
+++ qlearning.py
@@ -19,4 +19,7 @@ def QLQuestion1(iVal):
         print('  The discount factor (gamma) is 0.8')
         print('  The max action from state S2 is A3 with a value of 0.6')
         print('  0.1 + 0.1 * (5 + 0.8 * 0.6 - 0.1)')
-    
+
+def QLSolution1():
+    print('Here is my solution')
+    print('  Q[state][action] += alpha * (reward + gamma * Q[nstate].max() * (not done) - Q[state][action])')
